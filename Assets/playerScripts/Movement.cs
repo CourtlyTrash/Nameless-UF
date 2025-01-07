6d9a902a0febd43f5df9f7ebaadd78a456567a13
derpylama <larssonemil55@gmail.com>
2025-01-07 16:39:03 +0100

    started on movement

---
+++ Assets/playerScripts/Movement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    public float playerSpeed = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(horizontalInput, 0);
+        transform.Translate(movement  * playerSpeed * Time.deltaTime);
+    }
+}
